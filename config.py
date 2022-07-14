b38c7628bb79a0de4f390cb70a2348732b0a8172
do1e <dpj.email@qq.com>
2022-07-14 23:00:24 +0800

    add config

---
+++ config.py
@@ -0,0 +1,10 @@
+encrypt_js_script = "encrypt.js"
+configFile = "myconfig.json"
+outputFile = "remainElectric.out"
+outputFig = "remainElectric.png"
+agent = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Mobile Safari/537.36"
+urls = {
+	"login": "https://authserver.nju.edu.cn/authserver/login",
+	"captcha": "https://authserver.nju.edu.cn/authserver/captcha.html",
+	"electric": "https://wx.nju.edu.cn/njucharge/wap/electric/my"
+}
